54a9f87e13be2471a731a301a8e0434cd5b0bbf4
Pedro <pedrooconradoo@gmail.com>
2021-03-16 20:06:53 -0300

    Adding first C file
    - Adding philosophers.c
    - Creating the main function
    - Treating argument number error

---
+++ src/philosophers.c
@@ -0,0 +1,10 @@
+#include "philosophers.h"
+
+int main(int argc, char * args[]){
+    if(argc != 3)
+    {
+        printf("ERROR: Number of arguments is wrong. \nYou need enter 3 arguments.\n|Number of filoshophiers| Time to think| Time to Eat|");
+        abort();
+    }    
+}
+
